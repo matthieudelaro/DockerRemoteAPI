ed5ece81eb28c5dc220abcdac65271cbe7265668
matthieudelaro <matthieudelaro@gmail.com>
2016-01-01 23:09:45 +0100

    Initial Commit

---
+++ DockerRemoteAPI/DockerRemoteAPI.h
@@ -0,0 +1,19 @@
+//
+//  DockerRemoteAPI.h
+//  DockerRemoteAPI
+//
+//  Created by Matthieu on 1/1/16.
+//  Copyright © 2016 Matthieu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DockerRemoteAPI.
+FOUNDATION_EXPORT double DockerRemoteAPIVersionNumber;
+
+//! Project version string for DockerRemoteAPI.
+FOUNDATION_EXPORT const unsigned char DockerRemoteAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DockerRemoteAPI/PublicHeader.h>
+
+
